8250e116c1d277a779e1e8c7ee15240296afad6b
sevdas <sshukrieva@gmail.com>
2021-06-19 17:18:34 +0100

    initialize react app

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # likeable-client-app
-React application where users can vote for their favourite quote.
+
+React application where users can vote for their favorite quote.
---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from "./logo.svg";
+// import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Likeable-app</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
